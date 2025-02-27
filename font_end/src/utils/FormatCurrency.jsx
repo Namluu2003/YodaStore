78daa6239b11c884994f650ae3f2d48ec415380d
Thai Duy Duc <thaiduc20022310@gmail.com>
2025-02-27 22:35:25 +0700

    Xóa Folder font_end

--- font_end/src/utils/FormatCurrency.jsx
+++
@@ -1,8 +0,0 @@
-import numeral from "numeral";
-
-const FormatCurrency = ({value}) => {
-  // Định dạng số tiền thành tiền tệ Việt Nam (đ)
-  return numeral(value).format("0,0") + " đ";
-};
-
-export default FormatCurrency;
--- font_end/src/utils/format.js
+++
@@ -1,7 +0,0 @@
-import numeral from "numeral";
-
-export const formatCurrency = (value) => {
-  return numeral(value).format("0,0") + " đ";
-};
-
-export default formatCurrency;
