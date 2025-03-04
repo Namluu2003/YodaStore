5f4c53fb95d2932906e9c6d27d0b0e00ddbcbdfe
Thai Duy Duc <thaiduc20022310@gmail.com>
2025-03-04 07:20:35 +0700

    Render dữ liệu voucher

--- font_end/src/route.js
+++ font_end/src/route.js
@@ -1,16 +1,15 @@
-import BaseUI from './layouts/admin/BaseUI';
+import BaseUI from "./layouts/admin/BaseUI";
 
-import Color from './pages/admin/product/attribute/Color';
-import Size from './pages/admin/product/attribute/Size';
+// import Color from "./pages/admin/product/attribute/Color";
+// import Size from "./pages/admin/product/attribute/Size";
+import Voucher from "./pages/admin/voucher/Voucher";
 
 const publicRouters = [
-    { path: "/", element: BaseUI },
-    
-    
-    { path: "/admin/color", element: Color },
-    { path: "/admin/size", element: Size },
-    
-   
+  { path: "/", element: BaseUI },
+
+  // { path: "/admin/color", element: Color },
+  // { path: "/admin/size", element: Size },
+  { path: "/admin/voucher", element: Voucher },
 ];
 
 const privateRouters = [];
