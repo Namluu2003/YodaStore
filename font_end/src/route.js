be19dee660e4450872d5be530fb066448ad67b7b
Thai Duy Duc <thaiduc20022310@gmail.com>
2025-03-04 08:11:50 +0700

    Add form thêm mới voucher

--- font_end/src/route.js
+++ font_end/src/route.js
@@ -3,6 +3,7 @@ import BaseUI from "./layouts/admin/BaseUI";
 // import Color from "./pages/admin/product/attribute/Color";
 // import Size from "./pages/admin/product/attribute/Size";
 import Voucher from "./pages/admin/voucher/Voucher";
+import AddVoucher from "./pages/admin/voucher/AddVoucher";
 
 const publicRouters = [
   { path: "/", element: BaseUI },
@@ -10,6 +11,7 @@ const publicRouters = [
   // { path: "/admin/color", element: Color },
   // { path: "/admin/size", element: Size },
   { path: "/admin/voucher", element: Voucher },
+  { path: "/admin/voucher/add", element: AddVoucher },
 ];
 
 const privateRouters = [];
