a331ec82417298bdd6f60ac9384112b46533012a
Namluu2003 <tamdaonui1009@gmail.com>
2025-03-08 21:11:41 +0700

    done add sp

--- font_end/src/components/Admin/Product/AddShoeModal.jsx
+++ font_end/src/components/Admin/Product/AddShoeModal.jsx
@@ -1,4 +1,6 @@
 
+
+
 import React, { useEffect, useState } from "react";
 import * as request from "~/utils/httpRequest";
 import { toast } from "react-toastify";
@@ -133,8 +135,209 @@ function AddShoeModal({ onAddSuccess }) {
     setIsGalleryModalOpen(false);
   };
 
-  
- 
+  return (
+    <>
+      <Button
+        type="primary"
+        onClick={showModal}
+        className="bg-blue"
+        size="large"
+      >
+        <FaPlusCircle />
+      </Button>
+      <Modal
+        title="Thêm giày"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer=""
+        width={800}
+      >
+        <Form onFinish={handleOk} layout="vertical" form={form}>
+          <Form.Item
+            label="Tên giày"
+            name="name"
+            rules={[{ required: true, message: "Tên không được để trống!" }]}
+          >
+            <Input placeholder="Nhập tên giày..." />
+          </Form.Item>
+
+          <Form.Item
+            label="Thương hiệu"
+            name="brand"
+            rules={[
+              { required: true, message: "Thương hiệu không được để trống!" },
+            ]}
+          >
+            <Select
+              showSearch
+              optionFilterProp="children"
+              style={{ width: "100%" }}
+              onSearch={setSearchBrand}
+              placeholder="Chọn thương hiệu..."
+            >
+              {brandList.map((item) => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label="Đế"
+            name="sole"
+            rules={[{ required: true, message: "Đế không được để trống!" }]}
+          >
+            <Select
+              showSearch
+              optionFilterProp="children"
+              style={{ width: "100%" }}
+              onSearch={setSearchSole}
+              placeholder="Chọn đế..."
+            >
+              {soleList.map((item) => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label="Danh mục"
+            name="category"
+            rules={[
+              { required: true, message: "Danh mục không được để trống!" },
+            ]}
+          >
+            <Select
+              showSearch
+              optionFilterProp="children"
+              style={{ width: "100%" }}
+              onSearch={setSearchCate}
+              placeholder="Chọn danh mục..."
+            >
+              {cateList.map((item) => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="Hình ảnh" name="listImages">
+            <Button type="default" onClick={handleShowGallery}>
+              <FaPlusCircle /> Chọn từ Cloudinary
+            </Button>
+            {selectedImages.length > 0 && (
+              <div className="mt-2">
+                <p>Danh sách ảnh đã chọn</p>
+                <div className="d-flex flex-wrap gap-2">
+                  {selectedImages.map((url, index) => (
+                    <div
+                      key={index}
+                      style={{
+                        position: "relative",
+                        border: "2px dashed #fa8c16",
+                        padding: "5px",
+                      }}
+                    >
+                      <img
+                        src={url}
+                        alt={`selected-${index}`}
+                        style={{
+                          width: "100px",
+                          height: "100px",
+                          objectFit: "cover",
+                        }}
+                      />
+                      
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </Form.Item>
+
+          <div className="d-flex justify-content-end">
+            <Button type="primary" htmlType="submit">
+              <i className="fas fa-plus-circle me-1"></i> Thêm
+            </Button>
+          </div>
+        </Form>
+      </Modal>
+
+      {/* Modal cho ImageGallery */}
+      <Modal
+        title="Danh sách ảnh"
+        open={isGalleryModalOpen}
+        onCancel={handleCloseGalleryModal}
+        footer={[
+          <Button
+            key="close"
+            type="primary"
+            danger
+            onClick={handleCloseGalleryModal}
+          >
+            OK
+          </Button>,
+        ]}
+        width={800}
+      >
+        <div style={{ maxHeight: "400px", overflowY: "auto" }}>
+        {selectedImages.length > 0 && (
+              <div className="mt-2">
+                <p>Danh sách ảnh đã chọn</p>
+                <div className="d-flex flex-wrap gap-2">
+                  {selectedImages.map((url, index) => (
+                    <div
+                      key={index}
+                      style={{
+                        position: "relative",
+                        border: "2px dashed #fa8c16",
+                        padding: "5px",
+                      }}
+                    >
+                      <img
+                        src={url}
+                        alt={`selected-${index}`}
+                        style={{
+                          width: "100px",
+                          height: "100px",
+                          objectFit: "cover",
+                        }}
+                      />
+                      <button
+                        onClick={() => handleImageSelect(url, true)}
+                        style={{
+                          position: "absolute",
+                          top: "0",
+                          right: "0",
+                          background: "red",
+                          color: "white",
+                          border: "none",
+                          borderRadius: "50%",
+                          width: "20px",
+                          height: "20px",
+                          cursor: "pointer",
+                        }}
+                      >
+                        X
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          <ImageGallery
+          
+            onImageSelect={handleImageSelect}
+            selectedImages={selectedImages}
+          />
+        </div>
+      </Modal>
+    </>
+  );
 }
 
 export default AddShoeModal;
